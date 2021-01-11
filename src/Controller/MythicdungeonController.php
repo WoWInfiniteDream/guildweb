8d3ab1ea3d1708664d46e30d9e345628014f9eec
Richard Boer <richardboer@gmail.com>
2021-01-11 14:32:24 +0100

    Style change + raid progress
    Moved the styling to assets
    and fixed the raid progress part.
    Also fixed the menu

--- src/Controller/MythicdungeonController.php
+++ src/Controller/MythicdungeonController.php
@@ -11,6 +11,9 @@ class MythicdungeonController extends AbstractController
     /**
      * @Route("/mythicdungeon", name="mythicdungeon")
      */
+
+    //This page will pull the current data from wowprogress but have to decide what exactly will be pulled.
+    //Ideally, all of the users that want to show there data should be shown here. 
     public function index(): Response
     {
         return $this->render('mythicdungeon/index.html.twig', [
--- src/Controller/RaidProgressController.php
+++ src/Controller/RaidProgressController.php
@@ -26,6 +26,8 @@ class RaidProgressController extends AbstractController
     public function index(): Response
     {
         //Need to change this so that it pulls the data from the .env file, cause hardcoding realm, region and guild is dirty
+        //Maybe i should just pull the data from the database, that way, the global admin user can change those settings.
+        //Also should pull the data for the current raid from the API that way, no typo's can be made.
         $responseWowProgress = $this->client->request(
             'GET',
             'https://raider.io/api/v1/guilds/profile?region=eu&realm=argent-dawn&name=Infinite%20Dream&fields=raid_progression'
@@ -40,11 +42,31 @@ class RaidProgressController extends AbstractController
             $wowProgressGuild['data'] = json_decode($wowProgress_guild_json);
         }
 
+        $responseWowProgressRaidInfo = $this->client->request(
+            'GET',
+            'https://raider.io/api/v1/raiding/static-data?expansion_id=8'
+        );
+        //Checking what status code we get back
+        $statusCodeWowProgressRaidInfo = $responseWowProgressRaidInfo->getStatusCode();
+        if($statusCodeWowProgressRaidInfo == 200)
+        {
+            $wowProgress_raid_json = $responseWowProgressRaidInfo->getContent();
+
+            $wowProgressRaid['status'] = true;
+            $wowProgressRaid['data'] = json_decode($wowProgress_raid_json);
+            $arrayKeyRaid = array_key_last($wowProgressRaid['data']->raids);
+            $wowProgressRaid['data'] = $wowProgressRaid['data']->raids[$arrayKeyRaid];
+            $currentRaidSlug = $wowProgressRaid['data']->slug;
+        }
+
+        $wowCurrentProgressGuild = $wowProgressGuild['data']->raid_progression->$currentRaidSlug;
         //Need to build something so that i don't have to update the raid info every time a new raid comes out
 
         //Going to pull data from a few API's
         return $this->render('raid_progress/index.html.twig', [
             'wowProgressGuild' => $wowProgressGuild,
+            'wowProgressRaid' => $wowProgressRaid,
+            'currentProgress' => $wowCurrentProgressGuild,
         ]);
     }
 }
